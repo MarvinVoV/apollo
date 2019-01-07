0ba58098366e1084aa37264cf84a0f3701f7a450
MarvinVoV <marvin_vov@outlook.com>
2019-01-08 02:39:41 +0800

    add entity

---
+++ src/main/java/com/apollo/dao/entity/ArticleEntity.java
@@ -0,0 +1,23 @@
+package com.apollo.dao.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class ArticleEntity {
+    private long   id;
+    private long   userId;
+    private long   categoryId;
+    private String title;
+    private byte[] content;
+    private long   top;
+    private String tag;
+    private long   invisible;
+    private long   status;
+    private Date   gmtCreate;
+    private Date   gmtModified;
+}
